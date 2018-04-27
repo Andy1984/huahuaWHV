5bc1f9109ac6952751321486f184443497ee1a7f
yang.weicheng <445919439@qq.com>
2018-04-27 22:41:36 +1000

    能够成功获取用户名， 但传不出去

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -3,7 +3,14 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    chineseName:"",
+    familyName:"",
+    givenName:"",
+    australianPhone:"",
+    chinesePhone:"",
+    email:"",
+    entryTime:"",
+    wechat:"",
   },
   onLoad: function () {
     this.setData({
@@ -12,7 +19,55 @@ Page({
       })
     })
   },
-  gotoShowCode: function() {
-    console.log("goToShowCode")
-  }
+  
+  gotoShowCode: function(e) {
+    console.log(this.wechat)
+    console.log(this.data)
+    console.log(util.formatTime(new Date))
+  },
+  bindChineseName: function(e) {
+    this.setData({
+      chineseName: e.detail.value
+    })
+  },
+  bindWechat: function(e) {
+    this.setData({
+      wechat: e.detail.value
+    })
+  },
+  bindFamilyName: function(e) {
+    this.setData({
+      australianPhone: e.detail.value
+    })
+  },
+  bindGivenName: function (e) {
+    this.setData({
+      familyName: e.detail.value
+    })
+  },
+  bindAustralianPhone: function (e) {
+    this.setData({
+      australianPhone: e.detail.value
+    })
+  },
+  bindChinesePhone: function (e) {
+    this.setData({
+      chinesePhone: e.detail.value
+    })
+  },
+  bindEmail: function (e) {
+    this.setData({
+      email: e.detail.value
+    })
+  },
+  bindEntryTime: function (e) {
+    this.setData({
+      entryTime: e.detail.value
+    })
+  },
+  bindGivenName: function (e) {
+    this.setData({
+      familyName: e.detail.value
+    })
+  },
 })
